0afffb21bf79573f4bdc9b7679ea767c02e0e463
naveenraj <naveenraj.eternosoft.in>
2025-01-14 16:13:56 -0600

    Added favourite update and contact edit functionalities

---
+++ androidApp/src/main/java/com/example/contactssample/android/JsonParser.kt
@@ -0,0 +1,8 @@
+package com.example.contactssample.android
+
+class DataHelper {
+    fun parseTrainersList(json: String) {
+        fun <T> fromJson(json: String, type: Class<T>): T {}
+        fun <T> fromJsonArray(json: String, type: Class<Array<T>>): Array<T>
+    }
+}
--- shared/src/commonMain/kotlin/com/example/contactssample/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.example.contactssample
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
